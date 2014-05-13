0d57ca72ad02667b0691a36b91d7114edfb7e2bd
Kelsey Ishmael <kelsey.ishmael@lmnsolutions.com>
2014-05-13 16:55:25 -0400

    Removed excess css links from production build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -432,7 +432,6 @@ module.exports = function ( grunt ) {
         dir: '<%= compile_dir %>',
         src: [
           '<%= concat.compile_js.dest %>',
-          '<%= vendor_files.css %>',
           '<%= recess.compile.dest %>'
         ]
       }
