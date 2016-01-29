007b88f02368af69eae8e38453e12245d67f6bbf
travis <travislbrundage@gmail.com>
2016-01-29 10:01:42 -0800

    Update to js

--- bower-local/multilevel-menu/covermode.js
+++ bower-local/multilevel-menu/covermode.js
@@ -3,27 +3,6 @@ $(document).ready(function(){
 	$( '#menu' ).multilevelpushmenu({
 		//containersToPush: [$( '#pushobj' )],
 		mode: 'cover',
-		/*
-		onCollapseMenuEnd: function() {
-			$( '#map-container' ).css('width', '94%')
-		},
-		onExpandMenuStart: function() {
-			$( '#map-container' ).css('width', '74%')
-		}*/
 	});
-	// Make the map max size
-	var canvasheight=$('#map').parent().css('height');
-    var canvaswidth=$('#map').parent().css('width');
 
-    $( '#map' ).css('height', canvasheight);
-    $( '#map' ).css('width', canvaswidth);
-/*
-	// Making title editable
-	$( '#map-title' ).bind('dblclick', function() {
-		$(this).attr('contentEditable', true);
-	}).blur(function() {
-		$(this).attr('contentEditable', false);
-		// This needs to be fixed
-		mapService.title = $(this).val();
-	});*/
 });
