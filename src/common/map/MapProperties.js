74760cc56f5c3a644d623cee09a962db51be861d
travis <travislbrundage@gmail.com>
2016-02-03 09:05:18 -0800

    Front end for MapStory Summary

--- src/common/map/MapProperties.js
+++ src/common/map/MapProperties.js
@@ -4,6 +4,7 @@
 
   module.directive('loomMapProperties',
       function(mapService, configService, $translate) {
+        console.log(configService);
         return {
           templateUrl: 'map/partial/mapproperties.tpl.html',
           link: function(scope, element, attrs) {
