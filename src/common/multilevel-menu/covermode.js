c8e073d365ed1ee8b299e528b851849b5fbcbb92
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2016-02-10 00:04:35 -0500

    Move Menu to JS Array

--- src/common/multilevel-menu/covermode.js
+++ src/common/multilevel-menu/covermode.js
@@ -1,10 +1,31 @@
+var arrayMenu = [
+  {
+    title: '{{ storyService.title }}',
+    icon: 'fa fa-reorder',
+    items: [
+      {
+        name: '<button type="button" class="btn btn-default btn-md" data-target="#mapProperties" data-toggle="modal">Summary</button><button data-target="#mapSave" data-toggle="modal" type="button" class="btn btn-default btn-md">Save MapStory...</button> <button type="button" class="btn btn-default btn-md">Preview</button>',
+        link: '#'
+      },
+      {
+        name: '<a href="#" id="addchapter" ng-click="addChapter();"><i class="fa fa-plus-square-o"></i> Add a new chapter</a>',
+        link: '#'
+      },
+      {
+        name: '<a href="/getskills" target="_blank"><i class="fa fa-support"></i> Help</a>',
+        link: '#'
+      }
+    ]
+  }
+];
 $(document).ready(function() {
   // HTML markup implementation, cover mode
   $('#menu').multilevelpushmenu({
+    menu: arrayMenu,
     containersToPush: [$('pushobj')],
     mode: 'cover',
     onItemClick: function() {
-      $item = arguments[0];
+      $item = arguments[2];
       var idOfClicked = $item[0].id;
       // If the item has the id 'deleteChapter', then spawn the modal
       if (idOfClicked === 'deleteChapter') {
@@ -13,9 +34,6 @@ $(document).ready(function() {
       // If the item has the id 'addNewLayer', then spawn the modal
       if (idOfClicked === 'addNewLayer') {
         $('#add-layer-dialog').modal('show');
-      }
-      if ($(item.target).prop('tagName').toLowerCase() == 'input') {
-        $(item.target).focus();
       }
     },
     onCollapseMenuEnd: function() {
