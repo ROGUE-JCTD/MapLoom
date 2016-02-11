566e99f783756b5c40e6950fc75a7068948ccc7d
Clarence Davis Jr <cdavis@boundlessgeo.com>
2016-02-11 12:28:13 -0600

    Simplify the way boxes are handled based on the updated requirements. https://github.com/MapStory/mapstory/issues/1318

--- src/common/storybox/BoxService.js
+++ src/common/storybox/BoxService.js
@@ -1,165 +1,94 @@
-var SERVER_SERVICE_USE_PORT = false;
-var SERVER_SERVICE_USE_PROXY = true;
-
 (function() {
   var module = angular.module('loom_box_service', []);
-
-  // Private Variables
-  var servers = [];
-
+  var boxes_ = [];
+  var service_ = null;
   var rootScope_ = null;
-  //var location_ = null;
   var pulldownService_ = null;
-  var configService_ = null;
   var q_ = null;
   var httpService_ = null;
-  //var serverCount = 0;
+  var Box = function(data) {
+    ol.Feature.call(this, data);
+    this.start_time = getTime(this.start_time);
+    this.end_time = getTime(this.end_time);
+  };
+  Box.prototype = Object.create(ol.Feature.prototype);
+  Box.prototype.constructor = Box;
+  var model_attributes = ['id', '_id', 'title', 'description', 'start_time', 'end_time', 'extent'];
+
+  model_attributes.forEach(function(prop) {
+    Object.defineProperty(Box.prototype, prop, {
+      get: function() {
+        var val = this.get(prop);
+        return typeof val === 'undefined' ? null : val;
+      },
+      set: function(val) {
+        this.set(prop, val);
+      }
+    });
+  });
 
   module.provider('boxService', function() {
     this.$get = function($rootScope, $http, $q, $location, $translate, pulldownService, dialogService, configService) {
       service_ = this;
       rootScope_ = $rootScope;
-      dialogService_ = dialogService;
-      translate_ = $translate;
       httpService_ = $http;
-      location_ = $location;
       pulldownService_ = pulldownService;
-      configService_ = configService;
-      configService_.serverList = servers;
       q_ = $q;
-      //$rootScope.$on('map-created', function(event, config) {
-      console.log('----[ boxService, map created. initializing', config);
-      if (goog.isDefAndNotNull(config.id)) {
-        httpService_({
-          url: '/maps/' + config.id + '/boxes',
-          method: 'GET'
-        }).then(function(result) {
-          console.log(result);
-          var geojson = result.data;
-          geojson.features.map(function(f) {
-            var props = f.properties;
-            props.start_time *= 1000;
-            props.end_time *= 1000;
-            service_.addBox(props);
-          });
-        });
-      }
-      //});
 
-      return this;
+      // when a map is saved, save the boxes.
+      $rootScope.$on('map-saved', function(event, config) {
+        console.log('----[ boxService, notified that the map was saved', config);
+        httpService_.post('/maps/' + config.id + '/boxes', new ol.format.GeoJSON().writeFeatures(boxes_, {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'})).success(function(data) {
+          console.log('----[ boxService, saved. ', data);
+          return 'success';
+        });
+      });
+
+      $rootScope.$on('map-created', function(event, config) {
+        if (goog.isDefAndNotNull(config) && goog.isDefAndNotNull(config.id)) {
+          console.log('----[ boxService, map created. initializing', config);
+          httpService_({
+            url: '/maps/' + config.id + '/boxes',
+            method: 'GET'
+          }).then(function(result) {
+            console.log(result);
+            var geojson = result.data;
+            geojson.features.map(function(f) {
+              var props = f.properties;
+              props.start_time *= 1000;
+              props.end_time *= 1000;
+              var storyBox = new Box(props);
+              service_.addBox(storyBox);
+            });
+          });
+        }
+      });
+
+      return service_;
     };
 
     this.getBoxes = function() {
-      return servers;
+      return boxes_;
     };
 
-    this.addBox = function(boxInfo, loaded) {
-      var deferredResponse = q_.defer();
+    this.removeBox = function(storyBox) {
 
-      //boxInfo.extent = MapService.getView().getProjection().getExtent();
-
-      servers.push(boxInfo);
-      // save the config object on the server object so that when we save the server, we only pass the config as opposed
-      // to anything else that the app ads to the server objects.
-
-      if (goog.isDefAndNotNull(loaded)) {
-        loaded = false;
+      for (var i = 0; i < boxes_.length; i++) {
+        if (storyBox._id == boxes_[i]._id) {
+          boxes_.splice(i, 1);
+          break;
+        }
       }
 
-      rootScope_.$broadcast('box-added', boxInfo);
+    };
 
-      console.log('-- BoxService.addBox, added: ', boxInfo);
+    this.addBox = function(props, loaded) {
+      var deferredResponse = q_.defer();
+      var storyBox = new Box(props);
+      boxes_.push(storyBox);
+      rootScope_.$broadcast('box-added', storyBox);
+      console.log('-- BoxService.addBox, added: ', storyBox);
       pulldownService_.showStoryboxPanel();
-
-
-
-      /*
-      var doWork = function() {
-        console.log('---- MapService.layerInfo. trying to add server: ', server);
-        service_.populateLayersConfig(server)
-            .then(function(response) {
-              // set the id. it should always resolve to the length
-              if (goog.isDefAndNotNull(server.layersConfig) && server.layersConfig.length === 0 && !loaded &&
-                  server.lazy !== true) {
-                dialogService_.warn(translate_.instant('add_server'), translate_.instant('server_connect_failed'),
-                    [translate_.instant('yes_btn'), translate_.instant('no_btn')], false).then(function(button) {
-                  switch (button) {
-                    case 0:
-                      server.id = serverCount++;
-                      servers.push(server);
-                      rootScope_.$broadcast('server-added', server.id);
-                      deferredResponse.resolve(server);
-                      break;
-                    case 1:
-                      deferredResponse.reject(server);
-                      break;
-                  }
-                });
-              } else {
-                // If there are no layers on the server, layersConfig will be undefined.
-                if (!goog.isDefAndNotNull(server.layersConfig)) {
-                  server.layersConfig = [];
-                }
-                server.id = serverCount++;
-                servers.push(server);
-                rootScope_.$broadcast('server-added', server.id);
-                deferredResponse.resolve(server);
-              }
-            }, function(reject) {
-              deferredResponse.reject(reject);
-            });
-      };
-
-      if (goog.isDefAndNotNull(server.url)) {
-        if (server.url.indexOf(location_.host()) === -1) {
-          if (server.config.alwaysAnonymous) {
-            server.username = translate_.instant('anonymous');
-            server.authentication = undefined;
-            doWork();
-          } else {
-            dialogService_.promptCredentials(server.url, false, null).then(
-                function(credentials) {
-                  server.username = credentials.username;
-                  server.authentication = $.base64.encode(credentials.username + ':' + credentials.password);
-                  server.config.alwaysAnonymous = false;
-
-                  // remove the 'wms endpoint'
-                  var serverBaseUrl = removeUrlLastRoute(server.url);
-                  var serverAuthenticationUrl = serverBaseUrl + '/rest/settings.json';
-                  serverAuthenticationUrl = serverAuthenticationUrl.replace('http://', 'http://null:null@');
-                  ignoreNextScriptError = true;
-                  $.ajax({
-                    url: serverAuthenticationUrl,
-                    type: 'GET',
-                    dataType: 'jsonp',
-                    jsonp: 'callback',
-                    error: function() {
-                      ignoreNextScriptError = false;
-                    },
-                    complete: doWork
-                  });
-                }, function(reject) {
-                  server.username = translate_.instant('anonymous');
-                  server.authentication = undefined;
-                  server.config.alwaysAnonymous = reject.alwaysAnonymous;
-                  doWork();
-                });
-          }
-        } else {
-          server.username = configService_.username;
-          server.isLocal = true;
-
-          if (server.isVirtualService === true) {
-            server.name = 'Virtual Service';
-          } else {
-            server.name = translate_.instant('local_geoserver');
-          }
-
-          doWork();
-        }
-      } else {
-        doWork();
-      }*/
 
       return deferredResponse.promise;
     };
