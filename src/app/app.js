a888a60a13aae18d5ddfbeb64f092633d0381aa2
John Wileczek <john.wileczek@gmail.com>
2016-02-29 17:38:09 -0500

    Moving sidebar into main index.html still many styling issues that need to be worked out.

--- src/app/app.js
+++ src/app/app.js
@@ -73,179 +73,85 @@
         $scope.storyService = storyService;
         $scope.refreshService = refreshService;
 
+        $scope.mapstories = {
+          name: 'The Civil War',
+          chapters: [
+            {
+              id: 0,
+              chapter: 'Chapter 1',
+              title: 'Civil War Battle 1',
+              summary: 'This talks about Civil War Battle 1'
+            },
+            {
+              id: 1,
+              chapter: 'Chapter 2',
+              title: 'Civil War Battle 2',
+              summary: 'This talks about Civil War Battle 2'
+            },
+            {
+              id: 2,
+              chapter: 'Chapter 3',
+              title: 'Civil War Battle 3',
+              summary: 'This talks about Civil War Battle 3',
+              storyLayers: [
+                {
+                  id: 0,
+                  title: 'Civil War Chapter 3 StoryLayer 1'
+                },
+                {
+                  id: 1,
+                  title: 'Civil War Chapter 3 StoryLayer 2'
+                }
+              ],
+              storyBoxes: [
+                {
+                  id: 0,
+                  title: 'Civil War Chapter 3 Box 1'
+                },
+                {
+                  id: 1,
+                  title: 'Civil War Chapter 3 Box 2'
+                }
+              ],
+              storyPins: [
+                {
+                  id: 0,
+                  title: 'Civil War Chapter 3 Pin 1'
+                },
+                {
+                  id: 1,
+                  title: 'Civil War Chapter 3 Pin 2'
+                }
+              ]
+            }
+          ]
+        };
+
+        $scope.menuSection = 'mainMenu';
+
+        $scope.updateMenuSection = function(updateMenuSection) {
+          $scope.menuSection = updateMenuSection;
+        };
+
 
         $scope.addChapter = function() {
           //Add chapter to backend story service will return new chapter ID or null if failure
           var new_index = storyService.add_chapter();
           if (new_index !== null) {
-            $scope.addChapterToMenu(new_index);
-            // Expand to the chapter info form
-            var $expandTo = $(('#chapter-info-' + (new_index + 1)));
-            $scope.expandMenu($expandTo);
+            //TODO: Add calls to create new chapter in sidebar menu
           }
         };
 
         $scope.addStorylayerToMenu = function(chapter_index, layer_config) {
           console.log('layer to add:', layer_config);
-          var menu_element = $('#menu');
-          var html = "<ng-include src=\"'multilevel-menu/partials/storylayer.tpl.html'\"></ng-include>";
-          //var noDiv = "<div ng-include=\"'multilevel-menu/partials/storylayer.tpl.html'\"></div>";
-          var div = $('<div>');
-          div.html(html);
-          var storyLayer = $compile(div)($scope);
-
-          console.log(storyLayer);
-          if (!goog.isDefAndNotNull(layer_config.title)) {
-            return;
-          }
-          var story_layer_item = [
-            {
-              id: layer_config.title,
-              name: layer_config.title,
-              title: layer_config.title,
-              link: '#',
-              items: [
-                {
-                  name: storyLayer[0],
-                  title: layer_config.title,
-                  link: '#',
-                  items: []
-                }
-              ]
-            }
-          ];
-
-          var add_location = menu_element.multilevelpushmenu('findmenusbytitle', 'Chapter ' + (chapter_index) + ' StoryLayers');
-          if (add_location !== false) {
-            console.log('Adding layer to menu:', layer_config);
-            menu_element.multilevelpushmenu('additems', story_layer_item, add_location, 0);
-
-          } else {
-            console.log('Could not find StoryLayer subMenu for Chapter ', chapter_index);
-          }
+          //TODO: Add storylayer to sidebar menu based on chapter configuration.
 
         };
         //front end initialization of new chapter on menu element
         $scope.addChapterToMenu = function(index) {
-          var menu_element = $('#menu');
-          if (menu_element === null) {
-            return;
-          }
-          //human readable index
-          var readable_index = index + 1;
-
-          // Update the front end push menu
-          var $addTo = menu_element.multilevelpushmenu('activemenu').first();
-
-          //call to global function to setup chapter template I feel this should all be done some other way
-          var addChapter = create_chapter_template(readable_index);
-          menu_element.multilevelpushmenu('additems', addChapter, $addTo, index + 1);
-          // Bind the chapter title to the created menu object
-          var template = '<p> {{ storyService.configurations[' + index + '].about.title }} </p>';
-          var chapterTitle = $compile(angular.element(template))($scope);
-          $(chapterTitle).appendTo($(('#chapter' + (readable_index))));
-          // Bind the subtitle to the created chapter menu
-          template = '<h2> {{ storyService.configurations[' + index + '].about.title }} </h2>';
-          var subtitle = $compile(angular.element(template))($scope);
-          $(('#sub-chapter' + (readable_index) + ' > h2')).after(subtitle);
-
-          var story_layers = storyService.configurations[index].map.layers;//mapService.getLayers(true, true);
-          console.log('story_layers: ', story_layers);
-          for (var iLayer = 0; iLayer < story_layers.length; iLayer += 1) {
-            var story_layer = story_layers[iLayer];
-            $scope.addStorylayerToMenu(readable_index, story_layer);
-
-          }
+          //TODO: Add chapter to sidebar menu based on storyService.configurations[index]
 
         };
-
-        $scope.expandMenu = function(element_id) {
-
-          var element = $(element_id);
-          if (element === null) {
-            return;
-          }
-          $('#menu').multilevelpushmenu('expand', element);
-        };
-
-        $scope.initMenu = function() {
-          var titleTemplate = '<p>{{ storyService.title }}</p>';
-          var addChapterTemplate = '<a id="addchapter" ng-click="addChapter();">Add a new chapter</a>';
-          var title = $compile(angular.element(titleTemplate))($scope);
-          var addChapter = $compile(angular.element(addChapterTemplate))($scope);
-          var arrayMenu = [
-            {
-              title: title,
-              icon: 'fa fa-reorder',
-              items: [
-                {
-                  name: '<button type="button" class="btn btn-default btn-md" data-target="#mapProperties" data-toggle="modal">Summary</button> <button data-target="#mapSave" data-toggle="modal" type="button" class="btn btn-default btn-md">Save MapStory...</button> <button type="button" class="btn btn-default btn-md">Preview</button>',
-                  link: '#'
-                },
-                {
-                  name: addChapter,
-                  icon: 'fa fa-plus-square-o',
-                  link: '#'
-                },
-                {
-                  name: '<a href="/getskills" target="_blank">Help</a>',
-                  icon: 'fa fa-support',
-                  link: '#'
-                }
-              ]
-            }
-          ];
-          // HTML markup implementation, cover mode
-          $('#menu').multilevelpushmenu({
-            menu: arrayMenu,
-            containersToPush: [$('pushobj')],
-            mode: 'cover',
-            onItemClick: function() {
-              var $item = arguments[2];
-              var idOfClicked = $item[0].id;
-              var e = arguments[0];
-              // If the item has the id 'deleteChapter', then spawn the modal
-              if (idOfClicked === 'deleteChapter') {
-                $('#chapterDelete').modal('show');
-              }
-              // If the item has the id 'addNewLayer', then spawn the modal
-              if (idOfClicked === 'addNewLayer') {
-                $('#add-layer-dialog').modal('show');
-              }
-              if ($(e.target).prop('tagName').toLowerCase() == 'input' || $(e.target).prop('tagName').toLowerCase() == 'textarea') {
-                $(e.target).focus();
-              }
-            },
-            onGroupItemClick: function() {
-              // Update active config to tell it which chapter we're on using a zero based index.
-              //This function gets called whenever a menu element that has other items present under it is clicked
-              var $item = arguments[2];
-              var idOfClicked = $item[0].id;
-              //Check that we are clicking a base level chapter element first.
-              var chapter_match = idOfClicked.match(/^chapter(\d+)/);
-              if (chapter_match !== null) {
-                console.log(chapter_match);
-                var index = chapter_match[1] - 1;
-                storyService.update_active_config(index);
-              }
-            },
-            onCollapseMenuEnd: function() {
-              // Only if the entire menu is deactivated, expand map
-              var active = $('#menu').multilevelpushmenu('activemenu');
-              console.log(active);
-              if (active.prevObject.length === 0) {
-                $('#pushobj').css('width', '94%');
-              }
-            },
-            onExpandMenuStart: function() {
-              $('#pushobj').css('width', '74%');
-            }
-          });
-        };
-
-        //Initialize the multilevel menu and add first default chapter.
-        $scope.initMenu();
-        $scope.addChapterToMenu(0);
 
       });
 
