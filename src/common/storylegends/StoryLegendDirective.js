67c4391a734af100931f7c3b14c9cc7b495f9cfa
milafrerichs <mila.frerichs@gmail.com>
2016-03-01 16:22:53 +0100

    Change scope variable to layerAliases
    there was a conflict with another layers scope variable
    maybe we need a isolatedScope

--- src/common/storylegends/StoryLegendDirective.js
+++ src/common/storylegends/StoryLegendDirective.js
@@ -9,15 +9,14 @@
           replace: true,
           templateUrl: 'storylegends/partials/storylegend.tpl.html',
           link: function(scope) {
-            scope.layers = configService.configuration.map.layers;
             scope.$on('layer-added', function() {
               var layers = mapService.getLayers(false, true);
-              scope.layers = [];
+              scope.layerAliases = [];
               for (var i in layers) {
                 var layer = layers[i];
                 if (!layer.get('metadata').heatmap) {
                   var uniqueId = layer.get('metadata').uniqueID;
-                  scope.layers.push({
+                  scope.layerAliases.push({
                     id: uniqueId,
                     title: layer.get('metadata').config.titleAlias || layer.get('metadata').title,
                     layer: layer
@@ -26,8 +25,8 @@
               }
             });
             scope.saveMasking = function() {
-              for (var i in scope.layers) {
-                var layer = scope.layers[i];
+              for (var i in scope.layerAliases) {
+                var layer = scope.layerAliases[i];
                 var metadata = layer.layer.get('metadata');
                 metadata.config.titleAlias = layer.title;
                 layer.layer.set('metadata', metadata);
--- src/common/storylegends/StoryLegendDirectiveSpec.js
+++ src/common/storylegends/StoryLegendDirectiveSpec.js
@@ -1,5 +1,5 @@
 describe('StoryLegendDirective', function() {
-  var element, scope;
+  var element, scope, compiledElement;
   var createLayer = function(id, name, titleAlias) {
     data = {
       uniqueID: id,
@@ -43,7 +43,7 @@ describe('StoryLegendDirective', function() {
   beforeEach(inject(function($rootScope, $compile, $templateCache) {
     scope = $rootScope.$new();
     element = angular.element('<div class="loom-storylegend"></div>');
-    $compile(element)(scope);
+    compiledElement = $compile(element)(scope);
     scope.$digest();
   }));
 
@@ -61,20 +61,20 @@ describe('StoryLegendDirective', function() {
       expect(layers.length).toBe(2);
     });
     it('gets all layers', inject(function() {
-      expect(scope.layers.length).toBe(2);
+      expect(scope.layerAliases.length).toBe(2);
     }));
     it('the layers have an id', inject(function() {
-      expect(scope.layers[0]).toEqual(jasmine.objectContaining({
+      expect(scope.layerAliases[0]).toEqual(jasmine.objectContaining({
         id: 1
       }));
     }));
     it('the layers have a title', inject(function() {
-      expect(scope.layers[0]).toEqual(jasmine.objectContaining({
+      expect(scope.layerAliases[0]).toEqual(jasmine.objectContaining({
         title: 'Ocean Beach'
       }));
     }));
     it('the layers can have a titleAlias', inject(function() {
-      expect(scope.layers[1]).toEqual(jasmine.objectContaining({
+      expect(scope.layerAliases[1]).toEqual(jasmine.objectContaining({
         title: 'O.B.'
       }));
     }));
@@ -84,9 +84,9 @@ describe('StoryLegendDirective', function() {
       scope.$broadcast('layer-added');
     });
     it('saves the masking to the configration', function() {
-      scope.layers[0].title = 'Test';
+      scope.layerAliases[0].title = 'Test';
       scope.saveMasking();
-      expect(scope.layers[0].layer.get('metadata').config.titleAlias).toBe('Test');
+      expect(scope.layerAliases[0].layer.get('metadata').config.titleAlias).toBe('Test');
     });
   });
 });
