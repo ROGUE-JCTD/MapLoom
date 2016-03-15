d1e3175c8c38c40e64eb7e4a099ae63ccf8e50e7
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2016-03-15 09:58:24 -0400

    Add Angular Color Picker Dependency for the Edit Layer Styles Color Picker Tool

--- build.config.js
+++ build.config.js
@@ -66,6 +66,7 @@ module.exports = {
       'vendor/jquery-sortable/source/js/jquery-sortable-min.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
+      'vendor/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
