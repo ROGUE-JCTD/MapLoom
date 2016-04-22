a806e018f00909012fe84e733fdc167e95553086
John Wileczek <john.wileczek@gmail.com>
2016-04-22 12:27:02 -0400

    Simplified edit only mode to add through the config layer rather than add manually after composer has been loaded.

--- src/app/app.js
+++ src/app/app.js
@@ -273,25 +273,9 @@
           if (locationParameters['mode'] === 'edit') {
             // If edit mode is in the URL, get the value of the layer parameter.
             $scope.layerName = locationParameters['layer'];
-          }
-        };
-
-        $scope.$on('server-added', function(event, id) {
-          // If a server is added, get that server's ID.
-          var server = serverService.getServerById(id);
-          // Check if the server that was added was the Local GeoServer and we have a layer name because edit mode is active.
-          if (server.name == 'Local Geoserver' && goog.isDefAndNotNull($scope.layerName)) {
-            // Create minimal config for the layerName passed in the URL.
-            var minimalConfig = {
-              name: $scope.layerName,
-              source: server.id
-            };
-            // Add the layer with the minimal config we just created, and then update our menu section so that the user is in edit mode.
-            mapService.addLayer(minimalConfig);
             $scope.updateMenuSection('editSingleStoryLayer' + $scope.layerName);
           }
-        });
-
+        };
         $scope.addChapterToMenu(0);
 
         $scope.isEditModeActive();
