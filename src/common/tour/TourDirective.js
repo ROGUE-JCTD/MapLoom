7219a65051881840478e59655f040ba1889c2c67
Clarence Davis Jr <cldavisjr@gmail.com>
2016-07-15 07:30:07 -0500

    update the tour to remove the text mapstory

--- src/common/tour/TourDirective.js
+++ src/common/tour/TourDirective.js
@@ -20,14 +20,14 @@
                   target: 'mainMenu',
                   placement: 'bottom',
                   showNextButton: true,
-                  content: 'This is the sidebar where you will work to compose your MapStory.  Give your MapStory a title and click Save to begin.'
+                  content: 'This is the sidebar where you will work to compose your Story.  Give your Story a title and click Save to begin.'
                 },
                 {
                   // UpdateTourStep(1)
                   target: 'composerIconNav',
                   placement: 'right',
                   showNextButton: true,
-                  content: 'At any time, you can save your work or update your MapStory metadata using these icons.  When you\'re ready to publish your finished MapStory, you\'ll do that here.'
+                  content: 'At any time, you can save your work or update your Story metadata using these icons.  When you\'re ready to publish your finished Story, you\'ll do that here.'
                 },
                 {
                   // UpdateTourStep(2)
@@ -39,7 +39,7 @@
                   // UpdateTourStep(3)
                   target: 'chapterInfoButton',
                   placement: 'right',
-                  content: 'Each chapter has the same basic components.  Chapter Info, StoryLayers, StoryBoxes and StoryPins.  Let\'s get started by adding some Chapter Info, go ahead and click there now.'
+                  content: 'Each chapter has the same basic components.  Chapter Info, Layers, StoryBoxes and StoryPins.  Let\'s get started by adding some Chapter Info, go ahead and click there now.'
                 },
                 {
                   // UpdateTourStep(4)
@@ -51,54 +51,54 @@
                   // UpdateTourStep(5)
                   target: 'storyLayersButton',
                   placement: 'right',
-                  content: 'Now let\'s add StoryLayers to your Chapter.  StoryLayers are just our word for spatio-temporal datasets, and they form the foundation of your MapStory.  So pick your StoryLayers carefully!  ' +
-                            'Clicking on StoryLayers to the left will open a window where you can use StoryLayers you\'ve created, you\'ve favorited, or simply that you find by searching.  Once you\'ve added a StoryLayer or two, click on one of them so we can show you how to customize StoryLayers for your MapStory.'
+                  content: 'Now let\'s add Layers to your Chapter.  Layers are just our word for spatio-temporal datasets, and they form the foundation of your Story.  So pick your Layers carefully!  ' +
+                            'Clicking on Layers to the left will open a window where you can use Layers you\'ve created, you\'ve favorited, or simply that you find by searching.  Once you\'ve added a Layer or two, click on one of them so we can show you how to customize Layers for your MapStory.'
                 },
                 {
                   // UpdateTourStep(6)
                   target: 'addNewStoryLayerButton',
                   placement: 'bottom',
-                  content: 'Go ahead and click on Add a New StoryLayer... to choose which StoryLayer you want to add.'
+                  content: 'Go ahead and click on Add a New Layer... to choose which Layer you want to add.'
                 },
                 {
                   // UpdateTourStep(7)
                   target: 'storyLayersList',
                   placement: 'right',
-                  content: 'Now let\'s customize the StoryLayer you just added.  Click on the title of the StoryLayer.'
+                  content: 'Now let\'s customize the Layer you just added.  Click on the title of the Layer.'
                 },
                 {
                   // UpdateTourStep(8)
                   target: 'editFeatureButton',
                   placement: 'right',
-                  content: 'If you see any features that you think need to be added or changed in your StoryLayer, you can do that here. Remember, when you edit features in a StoryLayer, those changes will be visible to everyone on MapStory, not just you!',
+                  content: 'If you see any features that you think need to be added or changed in your Layer, you can do that here. Remember, when you edit features in a Layer, those changes will be visible to everyone, not just you!',
                   showNextButton: true
                 },
                 {
                   // UpdateTourStep(9)
                   target: 'editStoryLayerStyleButton',
                   placement: 'right',
-                  content: 'Each StoryLayer begins with a default style that you can customize for your MapStory. Currently MapStory supports simple, unique, choropleth and graduated styles. For point StoryLayers, you can also add icons using the Icons Commons.',
+                  content: 'Each Layer begins with a default style that you can customize for your Story. Currently Story supports Simple, Unique, Choropleth and Graduated styles. For point Layers, you can also add icons using the Icons Commons.',
                   showNextButton: true
                 },
                 {
                   // UpdateTourStep(10)
                   target: 'editStoryLayerMetaDataButton',
                   placement: 'right',
-                  content: 'Metadata will open up a new tab with all the additional information you might want to know about the StoryLayer (like, who created this StoryLayer? What’s the data source? When was it last updated?)',
+                  content: 'Metadata will open up a new tab with all the additional information you might want to know about the Layer (like, who created this Layer? What’s the data source? When was it last updated?)',
                   showNextButton: true
                 },
                 {
                   // UpdateTourStep(11)
                   target: 'viewStoryLayerTableButton',
                   placement: 'right',
-                  content: 'Table View shows you all the features in the StoryLayer in a table form. View History shows you a list of all the edits that have been made to the StoryLayer, and who made those edits.',
+                  content: 'Table View shows you all the features in the Layer in a table form. View History shows you a list of all the edits that have been made to the Layer, and who made those edits.',
                   showNextButton: true
                 },
                 {
                   // UpdateTourStep(12)
                   target: 'editStoryLayerMaskingButton',
                   placement: 'right',
-                  content: 'Finally, with Masking you can customize the name of the StoryLayer to make it more appropriate for your MapStory. The name you choose is what will appear in your Legend. Also, you can customize the features in your StoryLayer that you want to appear win a viewer clicks on your features, and how you want those features to be named. When you’re done, click Back to StoryLayers List and then click Back to Chapters List.'
+                  content: 'Finally, with Masking you can customize the name of the Layer to make it more appropriate for your Story. The name you choose is what will appear in your Legend. Also, you can customize the features in your Layer that you want to appear win a viewer clicks on your features, and how you want those features to be named. When you’re done, click Back to Layers List and then click Back to Chapters List.'
                 },
                 {
                   // UpdateTourStep(13)
@@ -142,7 +142,7 @@
                   // UpdateTourStep(19)
                   target: 'storyPinsButton',
                   placement: 'right',
-                  content: 'Now lets add some StoryPins. StoryPins are just what they sound like – pins on the map. Their purpose is to let you add more information on the map than might be included in your StoryLayers.  Click StoryPins to begin.'
+                  content: 'Now lets add some StoryPins. StoryPins are just what they sound like – pins on the map. Their purpose is to let you add more information on the map than might be included in your Layers.  Click StoryPins to begin.'
                 },
                 {
                   // UpdateTourStep(20)
@@ -187,7 +187,7 @@
                   // UpdateTourStep(26)
                   target: 'chapterInfoButton',
                   placement: 'right',
-                  content: 'Now you have StoryLayers, StoryBoxes, and StoryPins for your first chapter.  Click Back.'
+                  content: 'Now you have Layers, StoryBoxes, and StoryPins for your first chapter.  Click Back.'
                 },
                 {
                   // UpdateTourStep(27)
