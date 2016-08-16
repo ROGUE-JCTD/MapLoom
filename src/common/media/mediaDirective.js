c99833a101c0f6b205d7a207667e150c6230a9a3
John Wileczek <john.wileczek@gmail.com>
2016-08-16 13:47:15 -0400

    First pass of working embedded content through use of noembed service. Currently only allowing youtube need to add others.

--- src/common/media/mediaDirective.js
+++ src/common/media/mediaDirective.js
@@ -5,14 +5,28 @@
       function($translate, $http, $sce, $sanitize, mediaService) {
 
         return {
-          replace: false,
+          replace: true,
           restrict: 'E',
           template: '<div ng-bind-html="embedContent"></div>',
           link: function(scope, element, attrs) {
+            var embed_params = {
+              nowrap: 'on',
+              maxwidth: attrs.maxWidth,
+              maxheight: attrs.maxHeight
+            };
             scope.mediaService = mediaService;
+
+            attrs.$observe('maxWidth', function(value) {
+              embed_params.maxwidth = value;
+            });
+            attrs.$observe('maxHeight', function(value) {
+              embed_params.maxheight = value;
+            });
             attrs.$observe('src', function(value) {
               scope.mediaUrl = value;
-              scope.embedContent = $sce.trustAsHtml(mediaService.getEmbedContent(scope.mediaUrl, 180, 180));
+              mediaService.getEmbedContent(scope.mediaUrl, embed_params).then(function(result) {
+                scope.embedContent = $sce.trustAsHtml(result);
+              });
             });
           }
 
--- src/common/media/mediaService.js
+++ src/common/media/mediaService.js
@@ -3,37 +3,64 @@
   var service_ = null;
   var sce_ = null;
   var mediaHandlers_ = null;
+  var q_ = null;
+  var noembedProviders_ = null;
 
+
+  //ALERT: ANGULARJS WHITELIST IS BULLSHIT. The regex objects you provide get altered in various ways by angular upon
+  //creation of the whitelist. 1st it adds start and end anchor tokens to the regex, and then it ignores and flags you
+  //may have set for regex. This means all regex must be valid without use of case insensitivity flag (which sucks).
   module.config(function($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist([
       // Allow same origin resource loads.
       'self',
-      new RegExp('^(http[s]?):\/\/(w{3}.)?youtube.com\/.+$'),
-      new RegExp('^(http[s]?):\/\/(w{3}.)?soundcloud.com\/.+$'),
-      new RegExp('^(http[s]?):\/\/(w{3}.)?imgur.com\/.+$')
+      new RegExp(/https?:\/\/(?:w{3}\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com).+/im),
+      new RegExp(/https?:\/\/(w{3}\.)?soundcloud\.com\/.+/im),
+      new RegExp(/https?:\/\/(?:((?:m)\.)|((?:www)\.)|((?:i)\.))?imgur\.com\/?.+/im)
     ]);
 
   });
 
   module.provider('mediaService', function() {
 
-    this.$get = function($rootScope, $http, $sce, $translate) {
+    this.$get = function($rootScope, $http, $sce, $translate, $q) {
       sce_ = $sce;
       http_ = $http;
       translate_ = $translate;
+      q_ = $q;
       service_ = this;
+
+      http_.jsonp('https://noembed.com/providers?callback=JSON_CALLBACK', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(result) {
+        noembedProviders_ = result;
+      });
 
       mediaHandlers_ = service_.configureDefaultHandlers();
 
       return service_;
     };
 
+    this.isNOEmbedProvided = function(url) {
+      for (var iProvider = 0; iProvider < noembedProviders_.length; iProvider += 1) {
+        var provider = noembedProviders_[iProvider];
+        for (var iUrlScheme = 0; iUrlScheme < provider.patterns.length; iUrlScheme += 1) {
+          var regExp = new RegExp(provider.patterns[iUrlScheme], 'i');
+          if (url.match(regExp) !== null) {
+            return provider;
+          }
+        }
+      }
+      return null;
+    };
+
     this.configureDefaultHandlers = function() {
 
       var defaultHandlers = [
-        {name: 'youtube', regex: /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])/i, callback: embed_youtube},
-        {name: 'imgur', regex: /(?:https?:\/\/imgur\.com\/)[a-z]+\/(.*?)(?:[#\/].*|$)/i, callback: embed_imgur},
-        {name: 'soundcloud', regex: /(?:https?:\/\/imgur\.com\/)[a-z]+\/(.*?)(?:[#\/].*|$)/i, callback: embed_soundcloud}
+        //{name: 'youtube', regex: /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])/i, callback: embed_youtube},
+        {name: 'imgur', regex: /(https?:\/\/(\w+\.)?imgur\.com)/i, callback: embed_imgur}
       ];
 
       return defaultHandlers;
@@ -50,39 +77,98 @@
       return sce_.trustAsHtml(html);
     };
 
-    this.getEmbedContent = function(url, embed_width, embed_height) {
+    this.getEmbedContent = function(url, embed_params) {
 
-      var unsafeReturn = '<a href="' + url + '"> Unsafe Content </a>';
+      var unsafeReturn = '<a href="' + url + '"> Unable to Embed Content </a>';
 
+      //Check to see if we have a specialized handler first for this site
       for (var iHandler = 0; iHandler < mediaHandlers_.length; iHandler += 1) {
         var testHandler = mediaHandlers_[iHandler];
         if (testHandler.regex.test(url)) {
-          return testHandler.callback(url, embed_width, embed_height);
+          return testHandler.callback(url, embed_params);
         }
       }
 
+      //Check and see if the embed content is handled through the noembed service
+      if (service_.isNOEmbedProvided(url) !== null) {
+        return noembed_handler(url, embed_params);
+      }
+
+      //Unable to embed allowed content. Return a link to content.
       return unsafeReturn;
     };
 
     //Handler callbacks
-    function embed_youtube(url, embed_width, embed_height) {
+    function getNOEmbedRequestUrl(url, params) {
+      var api_url = 'https://noembed.com/embed?url=' + url + '&callback=JSON_CALLBACK',
+          qs = '',
+          i;
 
-      var regex = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig;
+      for (i in params) {
+        if (params[i] !== null) {
+          qs += '&' + encodeURIComponent(i) + '=' + params[i];
+        }
+      }
 
-      var embed = url.replace(regex,
-          '<iframe src="https://www.youtube.com/embed/$1" width="' + embed_width + '" height="' + embed_height + '" frameborder="0" allowfullscreen></iframe>');
+      api_url += qs;
+
+      return api_url;
+    }
+
+    function noembed_handler(url, embed_params) {
+
+      var response = q_.defer();
+
+      var request_url = getNOEmbedRequestUrl(url, embed_params);
+
+      http_.jsonp(request_url, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(result) {
+        response.resolve(result.html);
+      });
+
+      return response.promise;
+
+    }
+
+
+    function embed_youtube(url, embed_params) {
+
+      var response = q_.defer();
+
+      http_.jsonp('https://www.youtube.com/oembed?url=' + url + '&format=json&maxwidth=' + embed_params.maxwidth + '&maxheight=' + embed_params.maxheight, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(result) {
+        response.resolve(result.html);
+      });
+
+      return response.promise;
+    }
+
+    function embed_imgur(url, embed_params) {
+
+      var regex = /(https?:\/\/(\w+\.)?imgur\.com)/ig;
+
+      var matches = url.match(regex);
+
+      var embed = '';
+      if (matches.length > 1) {
+        //dealing with a basic image link from something like i.imgur.blah.png
+        embed = '<iframe src="' + url + '" width="' + embed_params.maxwidth + '" height="' + embed_params.maxheight + '"></iframe>';
+      } else {
+        //dealing with link to post or album
+        var id_regex = /https?:\/\/imgur\.com\/(?:\w+)\/?(.*?)(?:[#\/].*|$)/i;
+        embed = url.replace(id_regex,
+            '<blockquote class="imgur-embed-pub" lang="en" data-id="a/$1"></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>');
+      }
 
       return embed;
-    }
-
-    function embed_imgur(url, embed_width, embed_height) {
 
     }
-
-    function embed_soundcloud(url, embed_width, embed_height) {
-
-    }
-
 
   });
 
