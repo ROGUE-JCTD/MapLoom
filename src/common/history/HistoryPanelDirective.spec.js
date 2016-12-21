52761b346e2956eb853c994f19f7ce2d4b9a4c82
Dan 'Ducky' Little <danlittle@yahoo.com>
2016-12-21 10:19:20 -0600

    Cleaned up testing code a bit and added comments.

--- src/common/history/HistoryPanelDirective.spec.js
+++ src/common/history/HistoryPanelDirective.spec.js
@@ -3,7 +3,6 @@ describe('HistoryPanelDirective', function(){
   beforeEach(module('MapLoom'));
   beforeEach(module('loom_history'));
   beforeEach(module('history/partial/historypanel.tpl.html'));
-     //templateUrl: 'history/partial/historypanel.tpl.html',
 
   beforeEach(inject(function($rootScope, $compile, $templateCache, _configService_) {
     scope = $rootScope.$new();
@@ -15,46 +14,22 @@ describe('HistoryPanelDirective', function(){
 
   describe('Get Commit Author', function(){
     var anon = 'Anonymous';
+    // check whether Santa Claus exists.
     it('should return the name of the author', function() {
       var namen = 'santa claus';
       var author = compiledElement.scope().getCommitAuthor({author: {'name' : namen}});
       expect(author).toBe(namen);
     });
+    // Then check for an undefined author
     it('undefined author should return anonymous', function(){
       var author = compiledElement.scope().getCommitAuthor({});
       expect(author).toBe(anon);
     });
+    // finally check for a scenario in which the author's name 
+    //   is null.
     it('null author name should return anonymous', function(){
       var author = compiledElement.scope().getCommitAuthor({author: {'name' : null}});
       expect(author).toBe(anon);
     });
   });
-
-/*
-  describe('createOlFeatureBasedOnChange', function(){
-    it('should return a valid OpenLayers feature object', function(){
-      var diffLayer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          parser: null
-        })
-      });
-      var validationFeature;
-      var olChangeFeature = diffService.createOlFeatureBasedOnChange(modifiedChange, repoId);
-      diffLayer.getSource().addFeature(olChangeFeature);
-      diffLayer.getSource().forEachFeature(function(iterFeature){
-        if (iterFeature === olChangeFeature) {
-          validationFeature = iterFeature;
-        }
-      });
-      expect(olChangeFeature).toBe(validationFeature);
-    });
-  });
-
-  describe('findChangeLayer', function(){
-    it('should return the map layer to which the change object belongs', function(){
-      var testLayer = diffService.findChangeLayer(modifiedChange, repoId, [mockLayer]);
-      expect(testLayer).toBe(mockLayer);
-    });
-  });
-*/
 });
