6d2107371f9f9019f87e78270841447ed5404fb6
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-02-07 08:14:56 -0600

    Fix for undef'd authors throwing exception.
    Undefined Authors were throwing exceptions when trying to view
    the history panel. This commit fixes that by better handling
    cases when the author, author's name, author's email, or the timestamp
    is undefined.

--- src/common/history/HistoryPopoverDirective.js
+++ src/common/history/HistoryPopoverDirective.js
@@ -14,36 +14,46 @@
               element.popover('destroy');
               return;
             }
-            var safeName = function(name) {
-              if (goog.isDefAndNotNull(name) && name.length > 0) {
-                return name;
+            var safeName = function(author) {
+              if (goog.isDefAndNotNull(author)) {
+                if (goog.isDefAndNotNull(author.name) && author.name.length > 0) {
+                  return author.name;
+                }
               }
               return $translate.instant('anonymous');
             };
-            var safeEmail = function(email) {
-              if (goog.isDefAndNotNull(email) && email.length > 0) {
-                return email;
+            var safeEmail = function(author) {
+              if (goog.isDefAndNotNull(author)) {
+                if (goog.isDefAndNotNull(author.email) && author.email.length > 0) {
+                  return author.email;
+                }
               }
               return $translate.instant('no_email');
             };
-            var prettyTime = function(time) {
-              var date = new Date(time);
-              return date.toLocaleDateString() + ' @ ' + date.toLocaleTimeString();
+            var prettyTime = function(meta) {
+              if (goog.isDefAndNotNull(meta) && goog.isDefAndNotNull(meta.timestamp)) {
+                var date = new Date(meta.timestamp);
+                return date.toLocaleDateString() + ' @ ' + date.toLocaleTimeString();
+              }
+              return '';
             };
             var prettyMessage = function() {
-              return scope.commit.message;
+              if (goog.isDefAndNotNull(scope.commit.message)) {
+                return scope.commit.message;
+              }
+              return '';
             };
 
             var content = '<div class="popover-label">' + $translate.instant('author_name') + ':</div>' +
-                '<div class="popover-value">' + safeName(scope.commit.author.name) + '</div>' +
+                '<div class="popover-value">' + safeName(scope.commit.author) + '</div>' +
                 '<div class="popover-label">' + $translate.instant('author_email') + ':</div>' +
-                '<div class="popover-value">' + safeEmail(scope.commit.author.email) + '</div>' +
+                '<div class="popover-value">' + safeEmail(scope.commit.author) + '</div>' +
                 '<div class="popover-label">' + $translate.instant('committer_name') + ':</div>' +
-                '<div class="popover-value">' + safeName(scope.commit.committer.name) + '</div>' +
+                '<div class="popover-value">' + safeName(scope.commit.committer) + '</div>' +
                 '<div class="popover-label">' + $translate.instant('committer_email') + ':</div>' +
-                '<div class="popover-value">' + safeEmail(scope.commit.committer.email) + '</div>' +
+                '<div class="popover-value">' + safeEmail(scope.commit.committer) + '</div>' +
                 '<div class="popover-label">' + $translate.instant('commit_time') + ':</div>' +
-                '<div class="popover-value">' + prettyTime(scope.commit.committer.timestamp) + '</div>' +
+                '<div class="popover-value">' + prettyTime(scope.commit.committer) + '</div>' +
                 '<div class="popover-label">' + $translate.instant('message') + ':</div>' +
                 '<div class="popover-value">' + prettyMessage() + '</div>';
 
